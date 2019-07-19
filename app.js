3d4763637486501e97dd1d45808101b082050897
konstantin0s <constantintofan85@gmail.com>
2019-07-19 22:39:02 +0200

    Add port

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/randomBeer', (req, res, next) => {
 });
 
 
-const port = process.env.port || 5000;
-app.listen(5000, () => {
+const = process.env.port || 5000;
+app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
